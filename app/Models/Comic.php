96133b63088767fb895f19d954264279091b3236
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-06-18 16:26:01 +0200

    fix: added use softdeletes to the model

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comic extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['title', 'description', 'thumb', 'price', 'series','sale_date','type'];
+    use SoftDeletes;
+    protected $fillable = ['title', 'description', 'thumb', 'price', 'series', 'sale_date', 'type'];
 }
